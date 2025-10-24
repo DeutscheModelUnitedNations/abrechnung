fab60e09007a24c01a66eaa24797d882e06815ab
Tade Strehk <git@strehk.eu>
2025-10-24 17:23:59 +0200

    refactor: set default value for min parameter in costObject function

--- backend/models/helper.ts
+++ backend/models/helper.ts
@@ -22,7 +22,7 @@ export function costObject(
   required = false,
   defaultCurrency: string | null = null,
   defaultAmount: number | null = null,
-  min: number | undefined
+  min: number | undefined = undefined
 ) {
   // biome-ignore lint/suspicious/noExplicitAny: typing to complex
   const type: any = { amount: { type: Number, min, required: required, default: required && defaultAmount === null ? 0 : defaultAmount } }
